7eadc6f04827715c3d4af02dfe05d52e697d74ed
Ammar Ahmed Kiani <37160333+kiani007@users.noreply.github.com>
2024-04-18 10:09:51 +0000

    added project folder structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Routers from './Routers/Routers';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', width: '100vw' }}>
+      <h1>Welcome to Evoting App</h1>
+      <Routers />
+    </div>
+  );
+}
+
+export default App;
