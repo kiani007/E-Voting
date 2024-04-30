5a9b755910b20c7dc854218e689e7f15efccda3e
Kiani007 <ammarkiani30@gmail.com>
2024-04-30 17:41:12 +0500

    added icons and did dynamic format

--- src/pages/PresidentialCandidates/index.jsx
+++ src/pages/PresidentialCandidates/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Box,
   Card,
@@ -6,74 +6,100 @@ import {
   CardMedia,
   Grid,
   Typography,
-} from "@mui/material";
-import { Link } from "react-router-dom";
-import pti from "../../assets/Pti.png";
-import pml from "../../assets/pml.jpg";
+} from '@mui/material';
+import { Link } from 'react-router-dom';
+import pti from '../../assets/Pti.png';
+import pml from '../../assets/pml.jpg';
+import presidentialCandidatesData from './data';
+import vote from '../../assets/vote-img.png';
 
 const Index = () => {
   return (
-    <Box sx={{ display: "flex", justifyContent: "center", marginTop: "50px" }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Box sx={{ textAlign: "center" }}>
-            <Typography variant="h3" sx={{ fontWeight: "bold", mb: 2 }}>
-              General Election 2023
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Grid container spacing={2} justifyContent="center">
+        <Grid
+          item
+          xs={12}
+          sx={{ textAlign: 'center', bgcolor: 'primary.main' }}
+        >
+          <Typography
+            variant="h2"
+            align="center"
+            gutterBottom
+            sx={{
+              mt: 4,
+              fontWeight: 'bold',
+              color: 'white',
+              textTransform: 'uppercase',
+            }}
+          >
+            Presidential Candidates
+          </Typography>
+          <Box
+            bgcolor={'gray'}
+            sx={{
+              textAlign: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              mt: 4,
+              mb: 4,
+              margin: '0 auto',
+              bgcolor: 'white',
+              p: 2,
+            }}
+          >
+            <Typography
+              variant="h3"
+              sx={{ fontWeight: 'bold', mb: 2, color: 'primary.main' }}
+            >
+              General Election 2024
             </Typography>
-            <Typography variant="h5">Presidential Candidates</Typography>
           </Box>
         </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Card>
-            <CardMedia component="img" image={pml} alt="PML-N" height="150" />
-            <CardContent>
-              <Typography gutterBottom variant="h6" component="div">
-                Nawaz Sharif
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                PML-N
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Card>
-            <CardMedia component="img" image={pti} alt="PTI" height="150" />
-            <CardContent>
-              <Typography gutterBottom variant="h6" component="div">
-                Asif Zardari
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                PPP
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Card>
-            <CardMedia component="img" image={pti} alt="PTI" height="150" />
-            <CardContent>
-              <Typography gutterBottom variant="h6" component="div">
-                Altaf Hussain
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                MQM
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Card>
-            <CardMedia component="img" image={pti} alt="PTI" height="150" />
-            <CardContent>
-              <Typography gutterBottom variant="h6" component="div">
-                Imran Khan
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                PTI
-              </Typography>
-            </CardContent>
-          </Card>
+        <Grid
+          item
+          xs={12}
+          sx={{
+            p: 4,
+            m: 6,
+            bgcolor: 'primary.main',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            borderRadius: '10px',
+            color: 'white',
+          }}
+        >
+          <Grid container lg={12} spacing={2} justifyContent="center">
+            {presidentialCandidatesData.map((candidate) => (
+              <Grid item key={candidate.candidateId} lg={3} sx={{ p: 2 }}>
+                <Link
+                  to={`/candidate/${candidate.candidateId}`}
+                  style={{ textDecoration: 'none' }}
+                >
+                  <Card sx={{ maxWidth: 345, p: 4 }}>
+                    <CardMedia
+                      paddingTop="56.25%"
+                      component="img"
+                      height="350"
+                      width={'350'}
+                      image={candidate.candidateImage}
+                      alt={candidate.candidateName}
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h6" component="div">
+                        {candidate.candidateName}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {candidate.candidateParty}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Link>
+              </Grid>
+            ))}
+          </Grid>
         </Grid>
       </Grid>
     </Box>
