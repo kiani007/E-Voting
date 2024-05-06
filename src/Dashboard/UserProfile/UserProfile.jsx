c7876884c79be8f2d44819be8650665943734e8b
Kiani007 <ammarkiani30@gmail.com>
2024-05-07 03:43:20 +0500

    Refactor user Profile page

--- src/Dashboard/UserProfile/UserProfile.jsx
+++ src/Dashboard/UserProfile/UserProfile.jsx
@@ -17,13 +17,11 @@ import {
   FaEnvelope,
   FaPhone,
   FaVoteYea,
-  //   FaVoteNay,
   FaEdit,
 } from 'react-icons/fa';
 import { MdOutlineHowToVote } from 'react-icons/md';
 
 const ProfilePage = () => {
-  // Sample data for the user profile
   const [userProfile, setUserProfile] = useState({
     name: 'John Doe',
     cnic: '12345-6789101-1',
@@ -60,200 +58,204 @@ const ProfilePage = () => {
       style={{
         backgroundColor: '#f0f0f0',
         minHeight: '100vh',
-        padding: '20px',
+        display: 'flex',
+        borderRadius: '10px',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '3rem',
       }}
     >
-      <Container maxWidth="md">
-        <Paper
-          elevation={3}
-          style={{ padding: '20px', backgroundColor: '#fff' }}
-        >
-          <Grid container spacing={3} alignItems="center">
-            {/* Profile Picture Section */}
-            <Grid item xs={12} md={4}>
-              <div
-                style={{
-                  textAlign: 'center',
-                  backgroundColor: '#2196f3',
-                  padding: '20px',
-                  borderRadius: '10px',
-                }}
+      <Paper
+        elevation={3}
+        style={{
+          padding: '20px',
+          backgroundColor: '#fff',
+          width: '80%',
+          borderRadius: '10px',
+        }}
+      >
+        <Grid container spacing={3} alignItems="center">
+          {/* Profile Picture Section */}
+          <Grid item xs={12} md={4}>
+            <div
+              style={{
+                textAlign: 'center',
+              }}
+            >
+              <Avatar
+                style={{ width: '100px', height: '100px', margin: '0 auto' }}
               >
-                <Avatar
-                  style={{ width: '100px', height: '100px', margin: '0 auto' }}
-                >
-                  <FaUser style={{ fontSize: '60px' }} />
-                </Avatar>
-              </div>
-            </Grid>
-            {/* Name Section */}
-            <Grid item xs={12} md={8}>
-              <Typography variant="h4">{userProfile.name}</Typography>
-              {isEditing ? (
-                <TextField
-                  name="name"
-                  label="Name"
-                  fullWidth
-                  value={userProfile.name}
-                  onChange={handleChange}
-                  margin="normal"
-                />
-              ) : null}
-            </Grid>
-            {/* CNIC Section */}
-            <Grid item xs={12} md={4}>
-              <Typography variant="h6">
-                <FaIdCard /> CNIC
-              </Typography>
-              <Typography>{userProfile.cnic}</Typography>
-            </Grid>
-            {/* Address Section */}
-            <Grid item xs={12} md={8}>
-              <Typography variant="h6">
-                <FaAddressCard /> Address
-              </Typography>
-              {isEditing ? (
-                <>
-                  <TextField
-                    name="address1"
-                    label="Address 1"
-                    fullWidth
-                    value={userProfile.address1}
-                    onChange={handleChange}
-                    margin="normal"
-                  />
-                  <TextField
-                    name="address2"
-                    label="Address 2"
-                    fullWidth
-                    value={userProfile.address2}
-                    onChange={handleChange}
-                    margin="normal"
-                  />
-                </>
-              ) : (
-                <Typography>
-                  {userProfile.address1}
-                  <br />
-                  {userProfile.address2}
-                </Typography>
-              )}
-            </Grid>
-            {/* Father Name Section */}
-            <Grid item xs={12} md={4}>
-              <Typography variant="h6">Father's Name</Typography>
-              {isEditing ? (
-                <TextField
-                  name="fatherName"
-                  label="Father's Name"
-                  fullWidth
-                  value={userProfile.fatherName}
-                  onChange={handleChange}
-                  margin="normal"
-                />
-              ) : (
-                <Typography>{userProfile.fatherName}</Typography>
-              )}
-            </Grid>
-            {/* Phone Section */}
-            <Grid item xs={12} md={4}>
-              <Typography variant="h6">
-                <FaPhone /> Phone
-              </Typography>
-              {isEditing ? (
-                <TextField
-                  name="phone"
-                  label="Phone"
-                  fullWidth
-                  value={userProfile.phone}
-                  onChange={handleChange}
-                  margin="normal"
-                />
-              ) : (
-                <Typography>{userProfile.phone}</Typography>
-              )}
-            </Grid>
-            {/* Email Section */}
-            <Grid item xs={12} md={4}>
-              <Typography variant="h6">
-                <FaEnvelope /> Email
-              </Typography>
-              {isEditing ? (
-                <TextField
-                  name="email"
-                  label="Email"
-                  fullWidth
-                  value={userProfile.email}
-                  onChange={handleChange}
-                  margin="normal"
-                />
-              ) : (
-                <Typography>{userProfile.email}</Typography>
-              )}
-            </Grid>
-            {/* Voting Section */}
-            <Grid item xs={12} md={4}>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={userProfile.votedPresidential}
-                    icon={<MdOutlineHowToVote />}
-                    checkedIcon={<FaVoteYea />}
-                  />
-                }
-                label="Voted for Presidential"
-                disabled={!isEditing}
-              />
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={userProfile.votedVicePresidential}
-                    icon={<MdOutlineHowToVote />}
-                    checkedIcon={<FaVoteYea />}
-                  />
-                }
-                label="Voted for Vice Presidential"
-                disabled={!isEditing}
-              />
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={userProfile.eligible}
-                    icon={<MdOutlineHowToVote />}
-                    checkedIcon={<FaVoteYea />}
-                  />
-                }
-                label="Eligible"
-                disabled={!isEditing}
-              />
-            </Grid>
+                <FaUser style={{ fontSize: '60px' }} />
+              </Avatar>
+            </div>
           </Grid>
-          {isEditing ? (
-            <Button
-              variant="contained"
-              color="primary"
-              style={{ marginTop: '20px' }}
-              onClick={handleSave}
-            >
-              Save Changes
-            </Button>
-          ) : (
-            <Button
-              variant="outlined"
-              color="primary"
-              style={{ marginTop: '20px' }}
-              onClick={handleEdit}
-              startIcon={<FaEdit />}
-            >
-              Edit Profile
-            </Button>
-          )}
-        </Paper>
-      </Container>
+          {/* Name Section */}
+          <Grid item xs={12} md={8}>
+            <Typography variant="h4">{userProfile.name}</Typography>
+            {isEditing && (
+              <TextField
+                name="name"
+                label="Name"
+                fullWidth
+                value={userProfile.name}
+                onChange={handleChange}
+                margin="normal"
+              />
+            )}
+          </Grid>
+          {/* CNIC Section */}
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">
+              <FaIdCard /> CNIC
+            </Typography>
+            <Typography>{userProfile.cnic}</Typography>
+          </Grid>
+          {/* Address Section */}
+          <Grid item xs={12} md={8}>
+            <Typography variant="h6">
+              <FaAddressCard /> Address
+            </Typography>
+            {isEditing ? (
+              <>
+                <TextField
+                  name="address1"
+                  label="Address 1"
+                  fullWidth
+                  value={userProfile.address1}
+                  onChange={handleChange}
+                  margin="normal"
+                />
+                <TextField
+                  name="address2"
+                  label="Address 2"
+                  fullWidth
+                  value={userProfile.address2}
+                  onChange={handleChange}
+                  margin="normal"
+                />
+              </>
+            ) : (
+              <Typography>
+                {userProfile.address1}
+                <br />
+                {userProfile.address2}
+              </Typography>
+            )}
+          </Grid>
+          {/* Father Name Section */}
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Father's Name</Typography>
+            {isEditing ? (
+              <TextField
+                name="fatherName"
+                label="Father's Name"
+                fullWidth
+                value={userProfile.fatherName}
+                onChange={handleChange}
+                margin="normal"
+              />
+            ) : (
+              <Typography>{userProfile.fatherName}</Typography>
+            )}
+          </Grid>
+          {/* Phone Section */}
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">
+              <FaPhone /> Phone
+            </Typography>
+            {isEditing ? (
+              <TextField
+                name="phone"
+                label="Phone"
+                fullWidth
+                value={userProfile.phone}
+                onChange={handleChange}
+                margin="normal"
+              />
+            ) : (
+              <Typography>{userProfile.phone}</Typography>
+            )}
+          </Grid>
+          {/* Email Section */}
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">
+              <FaEnvelope /> Email
+            </Typography>
+            {isEditing ? (
+              <TextField
+                name="email"
+                label="Email"
+                fullWidth
+                value={userProfile.email}
+                onChange={handleChange}
+                margin="normal"
+              />
+            ) : (
+              <Typography>{userProfile.email}</Typography>
+            )}
+          </Grid>
+          {/* Voting Section */}
+          <Grid item xs={12} md={4}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={userProfile.votedPresidential}
+                  icon={<MdOutlineHowToVote />}
+                  checkedIcon={<FaVoteYea />}
+                  disabled={!isEditing}
+                />
+              }
+              label="Voted for Presidential"
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={userProfile.votedVicePresidential}
+                  icon={<MdOutlineHowToVote />}
+                  checkedIcon={<FaVoteYea />}
+                  disabled={!isEditing}
+                />
+              }
+              label="Voted for Vice Presidential"
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={userProfile.eligible}
+                  icon={<MdOutlineHowToVote />}
+                  checkedIcon={<FaVoteYea />}
+                  disabled={!isEditing}
+                />
+              }
+              label="Eligible"
+            />
+          </Grid>
+        </Grid>
+        {isEditing ? (
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ marginTop: '20px' }}
+            onClick={handleSave}
+          >
+            Save Changes
+          </Button>
+        ) : (
+          <Button
+            variant="outlined"
+            color="primary"
+            style={{ marginTop: '20px' }}
+            onClick={handleEdit}
+            startIcon={<FaEdit />}
+          >
+            Edit Profile
+          </Button>
+        )}
+      </Paper>
     </div>
   );
 };
