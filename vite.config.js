c83492089239dca7e22a9bc6253944600648fecd
Kiani007 <ammarkiani30@gmail.com>
2024-05-07 16:18:20 +0500

    added CI for deployment

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     minify: true,
   },
   define: {
-    'process.env': { NODE_ENV: '"production"' },
+    'process.env': { NODE_ENV: 'production' },
   },
   esbuild: {
     logOverride: { 'this-is-undefined-in-esm': 'silent' },
