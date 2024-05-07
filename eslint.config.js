9c7fa0bb26351bef1777da2833fde7efba00871d
Kiani007 <ammarkiani30@gmail.com>
2024-05-07 16:58:51 +0500

    fix lint issues

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,27 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
-export default [
-  {
-    languageOptions: { globals: globals.browser },
+// Es6 lint config
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'type-case': [2, 'always', 'lower-case'],
+    'scope-case': [2, 'always', 'lower-case'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
   },
-  pluginJs.configs.recommended,
-  pluginReactConfig,
-];
+};
